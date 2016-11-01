4e0279a7477743dd61006740f5f1226094451604
OAlienO <jeffrey6910@gmail.com>
2016-11-01 23:24:48 +0800

    Fix Disjoint-Set

--- Data-Structure/Disjoint-Set.cpp
+++ Data-Structure/Disjoint-Set.cpp
@@ -1,16 +1,6 @@
 struct DisjointSet{
-    int p[100],s[100];
-    void Init(int n){
-        for(int i=0;i<n;i++){
-            p[i] = i;
-            s[i] = 1;
-        }
-    }
+    int p[100];
+    void Init(int n){ for(int i=0;i<n;i++) p[i] = i; } // zero-based
     int Find(int x){ return x == p[x] ? x : p[x] = Find(p[x]); }
-    void Union(int x,int y){
-        int xx = Find(x),yy = Find(y);
-        if(xx == yy)return;
-        s[yy] += s[xx];
-        p[xx] = yy;
-    }
+    void Union(int x,int y){ p[Find(x)] = Find(y); }
 };
