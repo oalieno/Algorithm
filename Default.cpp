e60b956a50adda40106ec5a41ed454937f6ac4eb
OAlienO <jeffrey6910@gmail.com>
2016-12-20 00:59:14 +0800

    update

--- Default.cpp
+++ Default.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define IOS ios_base::sync_with_stdio(0);cin.tie(0)
 #define SZ(x) (int)x.size()
+#define pb push_back
 #define mp make_pair
 #define ff first
 #define ss second
@@ -13,6 +14,7 @@ typedef vector<PII> VPII;
 
 const int INF = 1000000000;
 const double PI = 3.1415926535897932384626;
+const double eps = 1e-7;
 
 #define int long long
 
