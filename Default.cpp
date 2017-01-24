8ed07f3906e3ee8af947453e5b0eb4c3ab7cc650
OAlienO <jeffrey6910@gmail.com>
2017-01-24 20:56:05 +0800

    update

--- Default.cpp
+++ Default.cpp
@@ -2,15 +2,13 @@
 
 using namespace std;
 
+#define endl '\n'
 #define IOS ios_base::sync_with_stdio(0);cin.tie(0)
 #define SZ(x) (int)x.size()
 #define pb push_back
 #define mp make_pair
 #define ff first
 #define ss second
-
-typedef pair<int,int> PII;
-typedef vector<PII> VPII;
 
 const int INF = 1000000000;
 const double PI = 3.1415926535897932384626;
