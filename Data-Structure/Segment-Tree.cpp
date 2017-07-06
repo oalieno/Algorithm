4d09c737a636e8ab4c82f6f0d825f5eb9eba48f0
OAlienO <jeffrey6910@gmail.com>
2017-07-06 10:08:41 +0800

    Update Segment-Tree.cpp

--- Data-Structure/Segment-Tree.cpp
+++ Data-Structure/Segment-Tree.cpp
@@ -1,15 +1,14 @@
-/* POJ 2777 Partial Code */
+/* HDU 1166 Partial Code */
 
 struct Node{
-    bitset<30> value; // need -1
-    int lazy;
+    int value,lazy;
     Node *lc,*rc;
     Node():lc(NULL),rc(NULL),lazy(0){}
-    void pull(){ value = lc->value | rc->value; }
+    void pull(){ value = lc->value + rc->value; }
     void push(){
         if(!lazy) return;
-        if(lc){ lc->lazy = lazy;lc->value.reset();lc->value.set(lazy-1); }
-        if(rc){ rc->lazy = lazy;rc->value.reset();rc->value.set(lazy-1); }
+        if(lc){ lc->lazy = lazy;lc->value += lazy; }
+        if(rc){ rc->lazy = lazy;rc->value += lazy; }
         lazy = 0;
     }
 };
@@ -19,7 +18,7 @@ int A[NUM];
 Node* build(int L,int R){
     Node *node = new Node();
     if(L == R){
-        node->value.reset();node->value.set(A[L]-1);
+        node->value = A[L];
         return node;
     }
     int mid = (L+R)>>1;
@@ -30,10 +29,10 @@ Node* build(int L,int R){
 }
 
 void modify(Node *node,int L,int R,int ql,int qr,int d){
-    if(R < ql || qr < L)return;
+    if(R < ql || qr < L) return;
     if(ql <= L && R <= qr){
-        node->lazy = d;
-        node->value.reset();node->value.set(d-1);
+        node->lazy += d;
+        node->value += d;
         return;
     }
     node->push();
@@ -43,10 +42,17 @@ void modify(Node *node,int L,int R,int ql,int qr,int d){
     node->pull();
 }
 
-bitset<30> query(Node* node,int L,int R,int ql,int qr){
-    if(R < ql || qr < L) return bitset<30>(0);
+int query(Node* node,int L,int R,int ql,int qr){
+    if(R < ql || qr < L) return 0;
     if(ql <= L && R <= qr) return node->value;
     node->push();
     int mid = (L+R)>>1;
-    return query(node->lc,L,mid,ql,qr) | query(node->rc,mid+1,R,ql,qr);
+    return query(node->lc,L,mid,ql,qr) + query(node->rc,mid+1,R,ql,qr);
+}
+
+void delete_(Node* now){
+    if(!now) return;
+    delete_(now->lc);
+    delete_(now->rc);
+    delete now;
 }
