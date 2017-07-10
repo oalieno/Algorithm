afb893a28d963fb13f125acc5027f6ddd2a02d1a
OAlienO <jeffrey6910@gmail.com>
2017-07-11 04:12:20 +0800

    Update Vector.cpp

--- Geometry/Vector.cpp
+++ Geometry/Vector.cpp
@@ -1,16 +1,16 @@
-struct Vector{
+struct VT{
     double x,y;
-    Vector(){}
-    Vector(double x, double y):x(x),y(y){}
-    Vector operator+(const Vector &v) const { return Vector(x+v.x,y+v.y); }
-    Vector operator-(const Vector &v) const { return Vector(x-v.x,y-v.y); }
-    Vector operator*(double c) const { return Vector(x*c,y*c); }
-    Vector operator/(double c) const { return Vector(x/c,y/c); }
-    double operator*(const Vector &v){ return x*v.x+y*v.y; }
-    double operator^(const Vector &v){ return x*v.y-y*v.x; }
+    VT(){}
+    VT(double x, double y):x(x),y(y){}
+    VT operator+(const VT &v) const { return VT(x+v.x,y+v.y); }
+    VT operator-(const VT &v) const { return VT(x-v.x,y-v.y); }
+    VT operator*(double c) const { return VT(x*c,y*c); }
+    VT operator/(double c) const { return VT(x/c,y/c); }
+    double operator*(const VT &v) const { return x*v.x+y*v.y; }
+    double operator^(const VT &v) const { return x*v.y-y*v.x; }
     double len(){ return sqrt(x*x+y*y); }
     double len2(){ return x*x+y*y; }
-    Vector U(){ return *this/len(); }
-    Vector T(){ return Vector(-y,x); }
-    Vector rot(double a){ return Vector(cos(a)*x-sin(a)*y,sin(a)*x+cos(a)*y); }
+    VT U(){ return *this/len(); }
+    VT T(){ return VT(-y,x); }
+    VT rot(double a){ return VT(cos(a)*x-sin(a)*y,sin(a)*x+cos(a)*y); }
 };
