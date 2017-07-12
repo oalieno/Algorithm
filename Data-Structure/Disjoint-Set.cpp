df47938bea413afbb9085535e7e9376cd355d10e
OAlienO <jeffrey6910@gmail.com>
2017-07-12 16:07:43 +0800

    Update Disjoint-Set.cpp

--- Data-Structure/Disjoint-Set.cpp
+++ Data-Structure/Disjoint-Set.cpp
@@ -1,6 +1,6 @@
-struct DisjointSet{
+struct DS{
     vector<int> p;
-    void Init(int n){ for(int i=0;i<n;i++) p.push_back(i); } // zero-based
-    int Find(int x){ return x == p[x] ? x : p[x] = Find(p[x]); } // Complexity : O(a(N))
-    void Union(int x,int y){ p[Find(x)] = Find(y); } // Complxity : O(a(N))
+    void init(int n){ for(int i=0;i<n;i++) p.push_back(i); } // zero-based
+    int find(int x){ return x == p[x] ? x : p[x] = find(p[x]); } // Complexity : O(a(N))
+    void union_(int x,int y){ p[find(x)] = find(y); } // Complxity : O(a(N))
 };
